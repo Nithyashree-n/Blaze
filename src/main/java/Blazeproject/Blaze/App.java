ac9abcecb36c2c13dc3476933bd0569f34cc5f33
Nithyashree-n <nithyashree.nithyanandam@atmecs.com>
2020-09-22 08:04:20 +0530

    Blaze

---
+++ src/main/java/Blazeproject/Blaze/App.java
@@ -0,0 +1,13 @@
+package Blazeproject.Blaze;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
